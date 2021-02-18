139868ff1b99081342959dd8e4c943608769bb49
koke <koke@kokenoMacBook-Air.local>
2021-02-18 23:44:22 +0900

    cs responsive

--- Assets/Scripts/Minigames/CyberSecurity/CyberSecurityManager.cs
+++ Assets/Scripts/Minigames/CyberSecurity/CyberSecurityManager.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CyberSecurityManager : MonoBehaviour
 {
-    public int x_max, y_max;
-    private int height, width;
+    public int x_max, y_max; // 縦横長さ設定（向き考えてない）
+    private int height, width; // 画面の向きに合わせた幅と高さ
 
-    public GameObject web_landscape;
-    public GameObject web_portrait;
+    public GameObject web_landscape; // 横向き用のWebのSprite
+    public GameObject web_portrait; // 縦向き用のWebのSprite
 
     // Start is called before the first frame update
     void Start()
     {
 
-        //現在のアスペクト比を取得
+        // アスペクト比によって表示を変更
         float currentAspect = (float)Screen.height / (float)Screen.width;
 
         if(currentAspect > 1)/* portrait */
@@ -30,9 +30,9 @@ public class CyberSecurityManager : MonoBehaviour
             web_portrait.SetActive(false);
         }
 
-
         GetComponent<CameraSizeUpdater>().setHeight(height);
         GetComponent<CameraSizeUpdater>().setWidth(width);
+
 
         StartCoroutine(createBeam());
     }
@@ -44,16 +44,14 @@ public class CyberSecurityManager : MonoBehaviour
     }
 
     IEnumerator createBeam()
-    {
-        
-
+    {        
         while (true)
         {
             float t = Random.Range(0.10f, 0.50f);
             yield return new WaitForSeconds(t);
 
-            int x = Random.Range(-x_max/4/2+2, x_max/4/2-2) * 4 + 2;
-            int y = Random.Range(-y_max / 6 / 2+2, y_max / 6 / 2-2) * 6 + 1;
+            int x = Random.Range(-width/4/2+2, width/4/2-2) * 4 + 2;
+            int y = Random.Range(-height / 6 / 2+2, height / 6 / 2-2) * 6 + 1;
 
             Vector3 position = new Vector3(x, y, 0);
             Instantiate(Resources.Load<GameObject>("Minigame/cybersecurity/cs_beam"), position, Quaternion.identity);
